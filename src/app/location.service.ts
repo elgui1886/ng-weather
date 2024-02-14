d335d58a98de2d2c45c5f0300aee380fdcf6d27b
Elia Guidi <eliaguidi@gipo.it>
2024-02-14 20:53:11 +0100

    wip: first step started

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -1,30 +1,32 @@
-import { Injectable } from '@angular/core';
-import {WeatherService} from "./weather.service";
+import { Injectable } from "@angular/core";
+import { WeatherService } from "./weather.service";
+import { BehaviorSubject } from "rxjs";
 
-export const LOCATIONS : string = "locations";
+export const LOCATIONS: string = "locations";
 
 @Injectable()
 export class LocationService {
+  private locations$ = new BehaviorSubject<string[]>([]);
 
-  locations : string[] = [];
+  location$ = this.locations$.asObservable();
 
-  constructor(private weatherService : WeatherService) {
+  locations: string[] = [];
+
+  constructor() {
     let locString = localStorage.getItem(LOCATIONS);
-    if (locString)
-      this.locations = JSON.parse(locString);
-    for (let loc of this.locations)
-      this.weatherService.addCurrentConditions(loc);
+    if (locString) this.locations = JSON.parse(locString);
+    this.locations$.next(this.locations);
   }
 
-  addLocation(zipcode : string) {
+  addLocation(zipcode: string) {
     this.locations.push(zipcode);
     localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
     this.weatherService.addCurrentConditions(zipcode);
   }
 
-  removeLocation(zipcode : string) {
+  removeLocation(zipcode: string) {
     let index = this.locations.indexOf(zipcode);
-    if (index !== -1){
+    if (index !== -1) {
       this.locations.splice(index, 1);
       localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
       this.weatherService.removeCurrentConditions(zipcode);
