877465a66bf28348d9d40f300c0aa7226d24e5ac
Elia Guidi <eliaguidi@gipo.it>
2024-02-15 00:54:52 +0100

    app_start

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -1,46 +1,33 @@
-import { Injectable } from "@angular/core";
-import { ReplaySubject, Subject } from "rxjs";
+import { Injectable } from '@angular/core';
+import {WeatherService} from "./weather.service";
 
-export const LOCATIONS: string = "locations";
-
-export type Location = {
-  zipcode: string;
-  action: 'add' | 'remove'
-};
+export const LOCATIONS : string = "locations";
 
 @Injectable()
 export class LocationService {
-  private _locations$ = new ReplaySubject<Location>();
-  location$ = this._locations$.asObservable();
-  locations: string[] = [];
 
-  constructor() {
+  locations : string[] = [];
+
+  constructor(private weatherService : WeatherService) {
     let locString = localStorage.getItem(LOCATIONS);
-    if (locString) this.locations = JSON.parse(locString);
-    this.locations.forEach(zipcode => this._locations$.next({
-      zipcode,
-      action: 'add'
-    }));
+    if (locString)
+      this.locations = JSON.parse(locString);
+    for (let loc of this.locations)
+      this.weatherService.addCurrentConditions(loc);
   }
 
-  addLocation(zipcode: string) {
+  addLocation(zipcode : string) {
     this.locations.push(zipcode);
     localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-    this._locations$.next({
-      zipcode,
-      action: 'add'
-    })
+    this.weatherService.addCurrentConditions(zipcode);
   }
 
-  removeLocation(zipcode: string) {
+  removeLocation(zipcode : string) {
     let index = this.locations.indexOf(zipcode);
-    if (index !== -1) {
+    if (index !== -1){
       this.locations.splice(index, 1);
       localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-      this._locations$.next({
-        zipcode,
-        action: 'remove'
-      })
+      this.weatherService.removeCurrentConditions(zipcode);
     }
   }
 }
